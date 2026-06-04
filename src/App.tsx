d4ab0db6c091396480fbb3cb93b43f1595eabad4
Your Name <you@example.com>
2026-06-04 20:44:10 +0300

    овововдввдвдв

--- src/App.tsx
+++ src/App.tsx
@@ -76,8 +76,18 @@ type TelegramWebApp = {
   };
 };
 
+type AppOrientationLockType =
+  | 'any'
+  | 'natural'
+  | 'landscape'
+  | 'portrait'
+  | 'portrait-primary'
+  | 'portrait-secondary'
+  | 'landscape-primary'
+  | 'landscape-secondary';
+
 type OrientationApi = ScreenOrientation & {
-  lock?: (orientation: OrientationLockType) => Promise<void>;
+  lock?: (orientation: AppOrientationLockType) => Promise<void>;
   unlock?: () => void;
 };
 
